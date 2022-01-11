6ec241cf7c05bd920f4002535ef65086cc2d9673
Amber M <amberm31@protonmail.com>
2022-01-11 09:28:48 -0600

    completed 21 merge linked links using recursion

--- 21-merge_two_sorted_lists.js
+++ 21-merge_two_sorted_lists.js
@@ -8,9 +8,34 @@
 // Output: [0]
 
 var mergeTwoLists = function (list1, list2) {
+    // If either list is empty, return the other list's node
+    if (list1 == null) return list2;
+    if (list2 == null) return list1;
+
+    // If l1 is lower
+    if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    }
+    // If l2 is lower
+    else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+    }
+};
+
+
+console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]))
+console.log(mergeTwoLists([-4, -2, -1], [-4, -3, -1]))
+console.log(mergeTwoLists([], []))
+console.log(mergeTwoLists([], [0]))
+
+
+/* The following three functions work for arrays but not linked lists: 
+
+var mergeTwoLists = function (list1, list2) {
     if (list1.length === 0) { return list2 }
     if (list2.length === 0) { return list1 }
-    let list3 = [];
     for (let i = 0; i < list1.length; i++) {
         for (let k = 0; k < list2.length; k++) {
             if (list2[k] <= list1[i]) {
@@ -22,21 +47,18 @@ var mergeTwoLists = function (list1, list2) {
     return list1;
 };
 
-console.log(mergeTwoLists([1, 2, 4], [1, 3, 4]))
-console.log(mergeTwoLists([-4, -2, -1], [-4, -3, -1]))
-console.log(mergeTwoLists([], []))
-console.log(mergeTwoLists([], [0]))
+
+var mergeTwoLists = function(list1, list2) {
+    let list3 = list1.concat(list2);
+    list3.sort(function(a,b){return a - b})
+    console.log(list3);    
+};
 
 
-// var mergeTwoLists = function(list1, list2) {
-//     let list3 = list1.concat(list2);
-//     list3.sort(function(a,b){return a - b})
-//     console.log(list3);    
-// };
+var mergeTwoLists = function(list1, list2) {
+    let list3 = [... list1, ...list2];
+    list3.sort(function(a,b){return a - b})
+    console.log(list3);    
+};
 
-
-// var mergeTwoLists = function(list1, list2) {
-//     let list3 = [... list1, ...list2];
-//     list3.sort(function(a,b){return a - b})
-//     console.log(list3);    
-// };
+*/
