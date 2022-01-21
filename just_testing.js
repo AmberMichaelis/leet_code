13da701d7012f18d56993b6ab44f59dc85843d16
Amber M <amberm31@protonmail.com>
2022-01-21 16:42:04 -0600

    added just testing file

---
+++ just_testing.js
@@ -0,0 +1,28 @@
+let runner = {
+    name: 'Runner',
+    run: function(speed) {
+        console.log(this.name + ' runs at ' + speed + ' mph.');
+    }
+};
+
+
+let flyer = {
+    name: 'Flyer',
+    fly: function(speed) {
+        console.log(this.name + ' flies at ' + speed + ' mph.');
+    }
+};
+
+let person = runner.run.bind(flyer, 20);
+person();
+
+let bird = flyer.fly(30)
+setTimeout(person,500);
+flyer.fly(40);
+
+let numbers = [0,1,2,3,4,5,6]
+console.log(numbers.map(timesTwo))
+
+var timesTwo = (num) => {
+    return num*2;
+}
