33404f6bd538fbed26668a21ca6b1d62ecaa7cfb
Amber M <amberswieczkowski@pm.me>
2022-06-06 23:02:23 -0400

    Progress on problem 53 - maximum subarray.

--- 53-maximum_subarray.js
+++ 53-maximum_subarray.js
@@ -19,11 +19,25 @@
  * @param {number[]} nums
  * @return {number}
  */
- var maxSubArray = function(nums) {
-    
+var maxSubArray = function (nums) {
+    if (nums.length === 0) return undefined;
+    if (nums.length === 1) return nums[0];
+    let currentTotal = 0;
+    let maxSum = nums[0];
+    for (let i = 0; i < nums.length; i++) {
+        currentTotal += nums[i]
+        if (currentTotal >= maxSum) {
+            maxSum = currentTotal
+            console.log(`currentTotal: ${currentTotal}. maxSum: ${maxSum}.`)
+        } else {
+            currentTotal = 0;
+        }
+    }
+    return maxSum
 };
 
-maxSubArray([-2,1,-3,4,-1,2,1,-5,4]) // 6
-maxSubArray([1]) // 1
-maxSubArray([5,4,-1,7,8]) // 23
+// console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])) // 6
+console.log(maxSubArray([1])) // 1
+console.log(maxSubArray([1, 3, -2, 5])) // 5
+// console.log(maxSubArray([5, 4, -1, 7, 8]))// 23
 
