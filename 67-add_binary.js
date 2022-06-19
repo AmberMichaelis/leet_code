10f85da5cf17ccdcc4c5a8795a4c4342957a14f0
Amber M <amberswieczkowski@pm.me>
2022-06-19 20:10:08 +0000

    Add comments for problem 67 Add Binary. Remove secondary solution.

--- 67-add_binary.js
+++ 67-add_binary.js
@@ -23,13 +23,15 @@ var addBinary = function (a, b) {
     let value = 0;
 
     for (let i = 0; i < max; i++) {
-        // adds binary digits starting with last place value, also adds one if there is a remainder
+        // adds last place value of a to last place value of b and repeats, also adds one if there is a remainder
         value = Number(a[lenA - 1 - i] || 0) + Number(b[lenB - 1 - i] || 0) + remainder;
         // rounds to 1 if you have a remainder, rounds to 0 if you don't have a remainder
         remainder = Math.floor(value / 2);
+        // result is 1 if value is 1, result is 0 if value is 0 or 2, converts answer to string
         result = (value % 2) + result;
     }
 
+    // adds a leading 1 if there was a remainder on final addition
     if (remainder) result = 1 + result;
 
     return result;
@@ -43,42 +45,5 @@ console.log(addBinary("10101010", "11001100")) // 101110110
 console.log(addBinary(
     "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
     "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"
-    )) // 110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000
+)) // 110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000
 
-
-
-
-// var addBinary = function(a, b) {
-//     if (a === "0" && b === "0" ) return "0";
-//     // convert a to regular number
-//     let exponent = 0;
-//     let digit;
-//     let valueOfA = 0;
-//     for (let i = a.length - 1; i >= 0; i--) {
-//         digit = a[i]
-//         valueOfA += digit * 2 ** exponent;
-//         exponent++
-//     }
-
-//     // convert b to regular number
-//     exponent = 0;
-//     let valueOfB = 0;
-//     for (let i = b.length - 1; i >= 0; i--) {
-//         digit = b[i]
-//         valueOfB += digit * 2 ** exponent;
-//         exponent++
-//     }
-//     // add a and b
-//     let sum = Number(valueOfA) + Number(valueOfB);
-//     console.log(`valueOfA: ${valueOfA}, valueOfB: ${valueOfB}, sum: ${sum}`)
-
-//     // convert sum to binary
-//     let array = [];
-//     while (sum > 0) {
-//             let remainder = sum % 2;
-//             array.unshift(remainder);
-//             sum = Math.floor(sum / 2);
-//             console.log(sum)
-//     }
-//     return array.join('')
-// };
