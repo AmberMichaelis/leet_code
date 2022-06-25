99f777597d74af6431435443f8a2f291da5a1c64
Amber M <amberswieczkowski@pm.me>
2022-06-25 22:21:37 +0000

    Start problem 70 climbing stairs.

---
+++ 70-climbing_stairs.js
@@ -0,0 +1,25 @@
+/* You are climbing a staircase. It takes n steps to reach the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * 
+ * Example 1:
+ * Input: n = 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 
+ * Example 2:
+ * Input: n = 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 
+ * Constraints:
+ * 1 <= n <= 45
+ * 
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var climbStairs = function(n) {
+    
+};
