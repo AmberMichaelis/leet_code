fb63722cc49e5863dc35903fd72e9d5ad5546a4a
Amber M <amberswieczkowski@pm.me>
2022-07-01 22:22:59 +0000

    Finish problem 70 climbing stairs.

--- 70-climbing_stairs.js
+++ 70-climbing_stairs.js
@@ -21,5 +21,22 @@
  * @return {number}
  */
  var climbStairs = function(n) {
-    
+    if (n == 1 || n == 0) return 1 // base cases
+
+    let first = 1; // num_ways(n-2)
+    let second = 2; // num_ways(n-1)
+
+    for (let i = 3; i <= n; i++) {
+
+        // num_ways(n) = num_ways(n-2) + num_ways(n-1)
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+    return second;
+
 };
+
+console.log(climbStairs(2)); // 2
+console.log(climbStairs(3)); // 3
+console.log(climbStairs(45)); // 1836311903
