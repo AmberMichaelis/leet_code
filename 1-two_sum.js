cad42e7048d0b8790cd17e0a1662174a5d0959bd
Amber M <amberswieczkowski@pm.me>
2022-07-24 22:17:46 +0200

    Update problem 1 Two Sum to include problem statement and rename variables.

--- 1-two_sum.js
+++ 1-two_sum.js
@@ -1,25 +1,35 @@
-// Input: nums = [2,7,11,15], target = 9
-// Output: [0,1]
-// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
-// Input: nums = [3,2,4], target = 6
-// Output: [1,2]
-
-// Input: nums = [3,3], target = 6
-// Output: [0,1]
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ * 
+ * Example 1:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * 
+ * Example 2:
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * 
+ * Example 3:
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ */
 
 var twoSum = function twoSum(nums, target) {
   let numObj = {};
-    for (let i = 0; i < nums.length; i++) {
-      let j = target - nums[i];
-      if (numObj[j] !== undefined) {
-        return [numObj[j], i];
-      }
-      numObj[nums[i]] = i;
+  for (let i = 0; i < nums.length; i++) {
+    let neededNum = target - nums[i];
+    if (numObj[neededNum] !== undefined) {
+      return [numObj[neededNum], i];
     }
+    numObj[nums[i]] = i;
   }
+}
 
-console.log(twoSum([1,2,4,5,8], 7))
+console.log(twoSum([2,7,11,15], 9)) // [0, 1]
+console.log(twoSum([3,2,4], 6)) // [1, 2]
+console.log(twoSum([3,3], 6)) // [0, 1]
 
 // Brute Force Method:
 
