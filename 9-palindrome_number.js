9ac63b9111e2c8435e6f4d57a77a6582410571d2
Amber M <amberswieczkowski@pm.me>
2022-07-31 21:45:56 +0200

    Problem 9 Palindrome Number: Add problem statement, rename variables, remove extra code.

--- 9-palindrome_number.js
+++ 9-palindrome_number.js
@@ -1,56 +1,35 @@
-// Input: x = 121
-// Output: true
-// Explanation: 121 reads as 121 from left to right and from right to left.
+/* Given an integer x, return true if x is palindrome integer.
+ * An integer is a palindrome when it reads the same backward as forward.
+ * For example, 121 is a palindrome while 123 is not.
+ * 
+ * Example 1:
+ * Input: x = 121
+ * Output: true
+ * Explanation: 121 reads as 121 from left to right and from right to left.
+ * 
+ * Example 2:
+ * Input: x = -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome
+ * 
+ * Example 3:
+ * Input: x = 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ */
 
-// Input: x = -121
-// Output: false
-// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
-
-// Input: x = 10
-// Output: false
-// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
-
-var isPalindrome = function(x) {
-    var s = '' + x;
-    var l = 0;
-    var r = s.length - 1;
-    while (l < r) {
-      if (s[l] !== s[r]) return false;
-      l++;
-      r--;
-    }
-    return true;
-  };
-
-// var isPalindrome = function(x) {
-//     var digit;
-//     var result = 0;
-//     var y = x;
-    
-//     if (x < 0) return false;
-    
-//     while(y) {
-//         digit = y % 10;
-//         result = ( result * 10 ) + digit;
-//         y = y/10|0
-//     }
-        
-//     return (x == result);
-    
-// };
-
-// var isPalindrome = function(x) {
-//     // Base condition
-//     if (x < 0) {
-//         return false;
-//     }
-//     // Store the number in a variable
-//     let number = x;
-//     // This will store the reverse of the number
-//     let reverse = 0;
-//     while (number > 0) {
-//         reverse = reverse * 10 + number % 10;
-//         number = parseInt(number / 10);
-//     }
-//     return x === reverse;
-// };
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  var number = '' + x;
+  var forwardIndex = 0;
+  var reverseIndex = number.length - 1;
+  while (forwardIndex < reverseIndex) {
+    if (number[forwardIndex] !== number[reverseIndex]) return false;
+    forwardIndex++;
+    reverseIndex--;
+  }
+  return true;
+};
