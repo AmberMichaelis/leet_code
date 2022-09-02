5198ac8e012f4fd4b85a0d5b93868cda70292bcd
Amber M <amberswieczkowski@pm.me>
2022-09-02 14:41:24 +0200

    Finish problem 125 and add explantion.

--- 125-valid_palindrome.js
+++ 125-valid_palindrome.js
@@ -21,7 +21,7 @@
  * Since an empty string reads the same forward and backward, it is a palindrome.
  * 
  * Constraints:
- * 1 <= s.length <= 2 * 105
+ * 1 <= s.length <= 2 * 10^5
  * s consists only of printable ASCII characters.
  */
 
@@ -30,5 +30,29 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
+    let str = s.match(/[A-Za-z0-9]/g) // returns an array or null if there is no match
+    if (!str) { return true }
+    str = str.join().toLowerCase();
 
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
+
+console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
+console.log(isPalindrome("race a car")); // false
+console.log(isPalindrome(" ")); // true
+
+/* Explanation:
+ * First, remove all non-alphanumeric characters. 
+ * If there are no alphanumeric characters, str is null.
+ * Otherwise, convert the array back into a string with join.
+ * Convert string to lower case letters.
+ * Iterate over all of the characters by comparing the
+ * beginning with the ending characters.
+ * Return false if the characters do not match.
+ * Return true if all of the characters match.
+ */
