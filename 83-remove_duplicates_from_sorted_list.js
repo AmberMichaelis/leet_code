5899975f68fe708297a9dfbf5a00ad62246959fe
Amber M <amberswieczkowski@pm.me>
2022-09-08 20:21:05 +0200

    Complete Problem 83 Remove Duplicates from Sorted List.

--- 83-remove_duplicates_from_sorted_list.js
+++ 83-remove_duplicates_from_sorted_list.js
@@ -1,5 +1,5 @@
-/* Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as
- * well.
+/* Given the head of a sorted linked list, delete all duplicates such that each element appears only once. 
+ * Return the linked list sorted as well.
  *
  * Example 1:
  * Input: head = [1,1,2]
@@ -17,43 +17,43 @@
  * 
  */
 
-const SinglyLinkedList = require('./singly_linked_list');
+// const SinglyLinkedList = require('./singly_linked_list');
+// also uncomment line 8 in singly_linked_list.js ... module.exports =
 
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
 var deleteDuplicates = function (head) {
-    if (!head || !head.next) return head;
-    let current = head;
-    let fast;
-    while (current) {
-        fast = current;
-        while (fast && current.val == fast.val) {
-            fast = fast.next;
+    let current = head
+    while (current !== null && current.next !== null) {
+        if (current.val === current.next.val) {
+            current.next = current.next.next
+        } else {
+            current = current.next
         }
-        current.next = fast;
-        current = fast;
-    }
 
+    }
+    return head
 };
 
-// Example 1
-let list1 = new SinglyLinkedList([1, 1, 2]);
-list1.deleteDuplicates();
-console.log(list1.print());
-
-// Example 2
-let list2 = new SinglyLinkedList([1, 1, 2, 3, 3]);
-list2.deleteDuplicates();
-console.log(list2.print());
-
-
-// list.addFirst(15);
-// list.addLast(35);
-// list.addFirst(5);
-// list.addAtIndex([1, 2, 3], 2);
-// list.addLast(45);
-// list.addAtIndex(25, 3);
-// console.log(list.removeAtIndex(4));
-// console.log(list.print());
+/* Explanation:
+ *
+ * In this explantion I use "second node" to refer to the next node,
+ * and "third node" to refer to the next next node.
+ * 
+ * Create a node called current that points to the head of the linked list.
+ * 
+ * While current node and second node are not null,
+ * 
+ * If the second node is equal to the third node,
+ * set the current node to point to the third node,
+ * thus skipping and removing the duplicate second node.
+ * If the second node is not equal to the third node,
+ * set the current node to the second node.
+ * Repeat the while loop.
+ * 
+ * If current or the next node are null,
+ * return the first node in the linked list.
+ *
+ */
