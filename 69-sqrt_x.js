3dc9859830d8262a1468d3b8fa09fc2845a700c6
Amber M <amberswieczkowski@pm.me>
2022-10-26 23:46:51 -0500

    Refactor problem 69 Sqrt x and add explanation.

--- 69-sqrt_x.js
+++ 69-sqrt_x.js
@@ -13,7 +13,7 @@
  * Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
  * 
  * Constraints:
- * 0 <= x <= 231 - 1
+ * 0 <= x <= 2^31 - 1
  */
 
 /**
@@ -21,31 +21,34 @@
  * @return {number}
  */
 var mySqrt = function (x) {
-    let left = 1;
-    let right = x;
-    // The square root of 0 or 1 is itself
     if (x < 2) return x;
+    let lower = 1;
+    let higher = x;
 
-    // Use binary search to find the square root or the whole number closest to the square root
-    while (left < right) {
-        // Find the middle point between left and right
-        const middle = Math.floor((left + right) / 2);
-        // Return the middle point if this is the square root
-        if (middle * middle === x) return middle;
-        // If middle squared is greater than x then the answer must be on the left half of middle
-        else if (middle * middle > x) {
-            right = middle;
+    while (lower < higher) {
+        const mid = Math.floor((lower + higher) / 2);
+        if (mid * mid === x) {
+            return mid;
+        } else if (mid * mid > x) {
+            higher = mid;
+        } else {
+            lower = mid + 1;
         }
-        // If middle squred is less than x then the answer must be on the right half of middle
-        else {
-            left = middle + 1;
-        }
-
     }
-    return left - 1;
+    return lower - 1;
 };
 
-console.log(mySqrt(0));
-console.log(mySqrt(4));
-console.log(mySqrt(8));
-console.log(mySqrt(12321));
+/* Explanation:
+ * Check if x is less than 2.
+ * If so, return x.
+ * Create a variable equal to 1, lower.
+ * Create a variable equal to x, higher.
+ * Create loop while lower is less than higher.
+ * Create a variable, mid, that adds lower + higher, divides by 2, then rounds down.
+ * Check if mid * mid equals x.
+ * If so, return mid.
+ * Check if mid * mid is greater than x.
+ * If so, set higher equal to mid and repeat while loop.
+ * Otherwise, set lower equal to mid + 1 and repeat while loop.
+ * Once lower is greater than or equal to higher, return lower - 1.
+ */
