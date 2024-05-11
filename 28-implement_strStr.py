281903e0e8fef15a5a45d6fadeb62781bfec4c14
Amber M <amberswieczkowski@pm.me>
2024-05-11 09:42:44 -0500

    Complete Problem 28 Find the Index of the First Occurrence in a String in Python.

---
+++ 28-implement_strStr.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+            return haystack.find(needle)
+
+solution = Solution()
+print(solution.strStr("sadbutsad","sad"))
+print(solution.strStr("leetcode","leeto"))
